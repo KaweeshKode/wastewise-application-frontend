11a0e11b36634cca33e682de339cbd74220956a9
KaweeshKode <gajinduksp@gmail.com>
2024-11-10 00:29:08 +0530

    navigation to signin and signup are done.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { SignupComponent } from './pages/signup/signup.component';
+import { LandingComponent } from './pages/landing/landing.component';
+import { SigninComponent } from './pages/signin/signin.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: "", component: LandingComponent},
+    { path: "signup", component: SignupComponent },
+    { path: "signin", component: SigninComponent }
+];
